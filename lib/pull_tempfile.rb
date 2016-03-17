8328d7a572dbb107ac9101f5f2fc6e1a1465517c
Tomas Valent <equivalent@eq8.eu>
2016-03-17 18:01:11 +0000

    PullTempfile.transfer

--- lib/pull_tempfile.rb
+++ lib/pull_tempfile.rb
@@ -1,5 +1,39 @@
+require 'httparty'     # gem
+require 'pathname'     # standard Ruby lib
+require 'tempfile'     # standard Ruby lib
+
 require "pull_tempfile/version"
 
 module PullTempfile
-  # Your code goes here...
+  # Creates Temporary file
+  #
+  #     file = PullTempfile.pull_tempfile(original_filename: "image asset.jpg", url: 'http://..../uaoeuoeueoauoueao' )
+  #     # ...do stuff
+  #     file.unlink
+  #
+  # Temporary files have suffix so your file "image asset.jpg"
+  # will be saved as "/tmp/image asset20160317-18066-gw1q0.jpg". This is because
+  # Tempfile lib is ensuring you won't override files
+  #
+  # Remmember to `file.unlink` or PullTempfile.transaction that does that automatically
+  #
+  def self.pull_tempfile(original_filename:, url:)
+    _generated_name = Pathname.new(original_filename)
+    extension     = _generated_name.extname.to_s
+    tmp_file_name = _generated_name.basename(extension).to_s
+
+    file = Tempfile.new([tmp_file_name, extension])
+    file.binmode
+    file.write(HTTParty.get(url).parsed_response)
+    file.close
+    file
+  end
+
+  def self.transaction(original_filename:, url:)
+    file = pull_tempfile(original_filename: original_filename, url: url)
+
+    yield file.open
+  ensure
+    file && file.unlink #delete temp file
+  end
 end
--- spec/pull_tempfile_spec.rb
+++ spec/pull_tempfile_spec.rb
@@ -5,7 +5,40 @@ describe PullTempfile do
     expect(PullTempfile::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  def use_vcr(&block)
+    VCR.use_cassette("basic", &block)
   end
+
+  let(:url) { 'http://www.eq8.eu/no-idea.png' }
+
+  it 'should execute block inside' do
+    use_vcr do
+      called = nil
+      described_class.transaction(url: url, original_filename: 'any name you desire.jpg') do |file|
+        called = file
+      end
+
+      expect(called).to be_kind_of File
+    end
+  end
+
+  it 'should delete file after transaction finish' do
+    use_vcr do
+      file_eval = nil
+
+      described_class.transaction(url: url, original_filename: 'any name you desire.jpg') do |file|
+        file_eval = file
+
+        expect(File.exist?(file_eval.path)).to be true
+
+        # file name will be like "/tmp/any name you desire20160317-19731-1gjqeq1.jpg"
+        regex = /\A\/tmp\/any name you desire\d{8}-.*\.jpg\z/
+        expect(file_eval.path.to_s).to match regex
+      end
+
+      expect(File.exist?(file_eval.path)).to be false
+    end
+  end
+
+
 end
